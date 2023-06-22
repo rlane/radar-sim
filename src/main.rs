d10a25aba6cf4b447d02820ed189a6844484cd0b
Rich Lane <lanerl@gmail.com>
2023-06-21 20:28:55 -0700

    preallocate buffers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::borrow::Cow;
 use std::sync::Arc;
-use wgpu::util::DeviceExt;
 use wgpu::SubmissionIndex;
 
 async fn run() {
@@ -32,7 +31,7 @@ async fn execute_gpu(numbers: &[f32]) {
         .await
         .unwrap();
 
-    let pass = create_radar_compute_pass(&device).await;
+    let pass = create_radar_compute_pass(&device, numbers.len()).await;
     let device = Arc::new(device);
 
     let result = execute_gpu_inner(&device, &queue, &pass, numbers)
@@ -55,9 +54,15 @@ async fn execute_gpu(numbers: &[f32]) {
 
 struct RadarComputePass {
     compute_pipeline: wgpu::ComputePipeline,
+    config_staging_buffer: wgpu::Buffer,
+    config_storage_buffer: wgpu::Buffer,
+    items_staging_buffer: wgpu::Buffer,
+    items_storage_buffer: wgpu::Buffer,
+    output_staging_buffer: wgpu::Buffer,
+    output_storage_buffer: wgpu::Buffer,
 }
 
-async fn create_radar_compute_pass(device: &wgpu::Device) -> RadarComputePass {
+async fn create_radar_compute_pass(device: &wgpu::Device, len: usize) -> RadarComputePass {
     // Loads the shader from WGSL
     let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
@@ -71,7 +76,61 @@ async fn create_radar_compute_pass(device: &wgpu::Device) -> RadarComputePass {
         entry_point: "main",
     });
 
-    RadarComputePass { compute_pipeline }
+    let config_size = std::mem::size_of::<u32>() as wgpu::BufferAddress;
+    let items_size = len as u64 * std::mem::size_of::<f32>() as wgpu::BufferAddress;
+    let output_size = (len * len) as u64 * std::mem::size_of::<f32>() as wgpu::BufferAddress;
+
+    let config_staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("config staging buffer"),
+        size: config_size,
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE,
+        mapped_at_creation: false,
+    });
+
+    let config_storage_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("config storage buffer"),
+        size: config_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    let items_staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("items staging buffer"),
+        size: items_size,
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE,
+        mapped_at_creation: false,
+    });
+
+    let items_storage_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("items storage buffer"),
+        size: items_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    let output_staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("output staging buffer"),
+        size: output_size,
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        mapped_at_creation: false,
+    });
+
+    let output_storage_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("output storage buffer"),
+        size: output_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    RadarComputePass {
+        compute_pipeline,
+        config_staging_buffer,
+        config_storage_buffer,
+        items_staging_buffer,
+        items_storage_buffer,
+        output_staging_buffer,
+        output_storage_buffer,
+    }
 }
 
 async fn execute_gpu_inner(
@@ -80,38 +139,6 @@ async fn execute_gpu_inner(
     pass: &RadarComputePass,
     numbers: &[f32],
 ) -> Option<Vec<f32>> {
-    let items_storage_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("items Storage Buffer"),
-        contents: bytemuck::cast_slice(numbers),
-        usage: wgpu::BufferUsages::STORAGE,
-    });
-
-    let config_storage_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("config Storage Buffer"),
-        contents: bytemuck::cast_slice(&[numbers.len() as u32]),
-        usage: wgpu::BufferUsages::STORAGE,
-    });
-
-    // Gets the size in bytes of the output buffer.
-    let output_size =
-        (numbers.len() * numbers.len()) as u64 * std::mem::size_of::<f32>() as wgpu::BufferAddress;
-
-    let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: None,
-        size: output_size,
-        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
-
-    let output_storage_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: Some("output Storage Buffer"),
-        size: output_size,
-        usage: wgpu::BufferUsages::STORAGE
-            | wgpu::BufferUsages::COPY_DST
-            | wgpu::BufferUsages::COPY_SRC,
-        mapped_at_creation: false,
-    });
-
     let bind_group_layout = pass.compute_pipeline.get_bind_group_layout(0);
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         label: None,
@@ -119,39 +146,72 @@ async fn execute_gpu_inner(
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource: items_storage_buffer.as_entire_binding(),
+                resource: pass.items_storage_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 1,
-                resource: output_storage_buffer.as_entire_binding(),
+                resource: pass.output_storage_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 2,
-                resource: config_storage_buffer.as_entire_binding(),
+                resource: pass.config_storage_buffer.as_entire_binding(),
             },
         ],
     });
 
-    // A command encoder executes one or many pipelines.
-    // It is to WebGPU what a command buffer is to Vulkan.
+    upload_buffer(
+        device,
+        &pass.config_staging_buffer,
+        bytemuck::cast_slice(&[numbers.len() as u32]),
+    )
+    .unwrap();
+
+    upload_buffer(
+        device,
+        &pass.items_staging_buffer,
+        bytemuck::cast_slice(numbers),
+    )
+    .unwrap();
+
     let mut encoder =
         device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    encoder.copy_buffer_to_buffer(
+        &pass.config_staging_buffer,
+        0,
+        &pass.config_storage_buffer,
+        0,
+        pass.config_storage_buffer.size(),
+    );
+
+    encoder.copy_buffer_to_buffer(
+        &pass.items_staging_buffer,
+        0,
+        &pass.items_storage_buffer,
+        0,
+        pass.items_storage_buffer.size(),
+    );
+
     {
         let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
         cpass.set_pipeline(&pass.compute_pipeline);
         cpass.set_bind_group(0, &bind_group, &[]);
         cpass.dispatch_workgroups(numbers.len() as u32 / 16, numbers.len() as u32 / 16, 1);
-        // Number of cells to run, the (x,y,z) size of item being processed
     }
-    // Sets adds copy operation to command encoder.
-    // Will copy data from storage buffer on GPU to staging buffer on CPU.
-    encoder.copy_buffer_to_buffer(&output_storage_buffer, 0, &staging_buffer, 0, output_size);
+
+    encoder.copy_buffer_to_buffer(
+        &pass.output_storage_buffer,
+        0,
+        &pass.output_staging_buffer,
+        0,
+        pass.output_storage_buffer.size(),
+    );
 
     let start_time = std::time::Instant::now();
 
     // Submits command encoder for processing
     let id = queue.submit(Some(encoder.finish()));
-    let buffer_slice = staging_buffer.slice(..);
+    let buffer_slice = pass.output_staging_buffer.slice(..);
     let data = map_buffer(&device, &buffer_slice, id).unwrap();
 
     if false {
@@ -165,11 +225,11 @@ async fn execute_gpu_inner(
     // With the current interface, we have to make sure all mapped views are
     // dropped before we unmap the buffer.
     drop(data);
-    staging_buffer.unmap(); // Unmaps buffer from memory
-                            // If you are familiar with C++ these 2 lines can be thought of similarly to:
-                            //   delete myPointer;
-                            //   myPointer = NULL;
-                            // It effectively frees the memory
+    pass.output_staging_buffer.unmap(); // Unmaps buffer from memory
+                                        // If you are familiar with C++ these 2 lines can be thought of similarly to:
+                                        //   delete myPointer;
+                                        //   myPointer = NULL;
+                                        // It effectively frees the memory
 
     // Returns data from buffer
     return Some(result);
@@ -192,4 +252,23 @@ fn map_buffer<'a>(
     device.poll(wgpu::Maintain::WaitForSubmissionIndex(id));
     receiver.recv().unwrap()?;
     Ok(buffer_slice.get_mapped_range())
+}
+
+fn upload_buffer(
+    device: &wgpu::Device,
+    buffer: &wgpu::Buffer,
+    data: &[u8],
+) -> Result<(), wgpu::BufferAsyncError> {
+    let (sender, receiver) = std::sync::mpsc::channel();
+    let buffer_slice = buffer.slice(..);
+    buffer_slice.map_async(wgpu::MapMode::Write, move |v| {
+        drop(sender.send(v));
+    });
+    device.poll(wgpu::Maintain::Wait);
+    receiver.recv().unwrap()?;
+    let mut view = buffer_slice.get_mapped_range_mut();
+    view.copy_from_slice(data);
+    drop(view);
+    buffer.unmap();
+    Ok(())
 }
